c64e766ecc501fed392e5dbdf188ed95def0074b
Andre Losse <andreloze@gmail.com>
2022-09-12 12:13:36 -0300

    Initial commit

---
+++ adoptions/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Pets(models.Model):
+
+    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=57)
+    breed = models.CharField(max_length=57)
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+    age = models.IntegerField(null=True)
+    submission_date = models.DateTimeField()
+    description = models.TextField()
+    vaccinations = models.ManyToManyField('Vaccines', blank=True)
+
+class Vaccines(models.Model):
+
+    name = models.CharField(max_length=57)
