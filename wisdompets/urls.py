91be77760c0840877ea10b3771a75b07da7bb596
Andre Losse <andreloze@gmail.com>
2022-09-12 17:33:25 -0300

    Added templates

--- wisdompets/urls.py
+++ wisdompets/urls.py
@@ -6,5 +6,5 @@ from adoptions import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('adoptions/<int:pet_id>/', views.pet_detail, name='pet_detail')
+    path('adoptions/<int:pet_id>/', views.pet_details, name='pet_details')
 ]
