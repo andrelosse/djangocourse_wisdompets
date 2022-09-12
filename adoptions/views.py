0f04b8d32c77600e566f435af3a824cefe5f6e85
Andre Losse <andreloze@gmail.com>
2022-09-12 16:29:52 -0300

    Added views

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,2 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pets
+
+def index (request):
+    pets = Pets.objects.all()
+
+    return render(request, 'index.html', {'pets': pets,})
+
+def pet_details (request, pet_id):
+
+    try:
+        pet = Pets.objects.get(id=pet_id)
+    except Pets.DoesNotExist:
+        raise Http404('Pet does not exist...')
+    
+    return render(request, 'pet_details.html', {'pet': pet,})
